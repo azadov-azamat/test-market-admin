82488724596a0ec2a6809890d405628c1120c84f
azadov-azamat <azamatazadov112@gmail.com>
2023-10-02 17:36:57 +0500

    product crud edit

--- src/views/products/create-product.jsx
+++ src/views/products/create-product.jsx
@@ -6,16 +6,17 @@ import {INPUT_MSG} from "../../utility/Utils"
 import {unwrapResult} from "@reduxjs/toolkit"
 import {createProduct, patchProduct, setProduct} from "../../redux/reducers/product"
 import FileUploaderSingle from "../../components/FileUploaderSingle"
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import Button from '../../components/Btn'
 import {uploadFile} from "../../redux/reducers/file"
-import {getStores, setStore} from "../../redux/reducers/store"
+import {setStore} from "../../redux/reducers/store"
 import Select from "react-select"
 import {getAddresses} from "../../redux/reducers/address"
 
 export default function CreateProduct({
                                           toggleModal,
-                                          modal
+                                          modal,
+                                          storeId
                                       }) {
 
     const dispatch = useDispatch()
@@ -23,16 +24,20 @@ export default function CreateProduct({
         product,
         isLoading
     } = useSelector(state => state.products)
-    const {stores} = useSelector(state => state.stores)
     const {addresses} = useSelector(state => state.addresses)
 
     const [file, setFile] = useState(null)
+
+    useEffect(() => {
+        if (product) {
+            dispatch(getAddresses())
+        }
+    }, [product])
 
     const ValidateSchema = Yup.object().shape({
         productName: Yup.string().required(INPUT_MSG),
         productPrice: Yup.string().required(INPUT_MSG),
         productQuantity: Yup.string().required(INPUT_MSG),
-        storeId: Yup.string().required(INPUT_MSG),
         adressId: Yup.string().required(INPUT_MSG),
         productMeasure: Yup.string().required(INPUT_MSG)
     })
@@ -42,7 +47,7 @@ export default function CreateProduct({
             productName: product?.productName || '',
             productPrice: product?.productPrice || '',
             productQuantity: product?.productQuantity || '',
-            storeId: product?.storeId || '',
+            storeId,
             adressId: product?.adressId || '',
             productMeasure: product?.productMeasure || ''
         },
@@ -136,7 +141,7 @@ export default function CreateProduct({
                                     id={'productModel'}
                                     name={"productModel"}
                                     defaultValue={product?.productModel}
-                                    type={"text"}
+                                    type={"textarea"}
                                     onChange={formik.handleChange}
                                 />
                             </div>
@@ -165,35 +170,35 @@ export default function CreateProduct({
                                 />
                             </div>
                         </Col>
-                        <Col>
-                            <div className="mb-1">
-                                <Label className="form-label" for="storeId">
-                                    Do'kon tanlang
-                                </Label>
-                                <Select
-                                    id="storeId"
-                                    name="storeId"
-                                    placeholder="Tanlang..."
-                                    onFocus={() => {
-                                        dispatch(getStores())
-                                    }}
-                                    options={stores}
-                                    // defaultValue={{
-                                    //     label: currentItem?.active ? "active" : "inactive",
-                                    //     value: currentItem?.active
-                                    // }}
-                                    getOptionLabel={option => option.storeName}
-                                    getOptionValue={option => option.id}
-                                    onChange={(val) => {
-                                        formik.setFieldValue("storeId", val.id)
-                                    }}
-                                />
-                            </div>
-                        </Col>
+                        {/*<Col>*/}
+                        {/*    <div className="mb-1">*/}
+                        {/*        <Label className="form-label" for="storeId">*/}
+                        {/*            Do'kon tanlang*/}
+                        {/*        </Label>*/}
+                        {/*        <Select*/}
+                        {/*            id="storeId"*/}
+                        {/*            name="storeId"*/}
+                        {/*            placeholder="Tanlang..."*/}
+                        {/*            onFocus={() => {*/}
+                        {/*                dispatch(getStores())*/}
+                        {/*            }}*/}
+                        {/*            options={stores}*/}
+                        {/*            // defaultValue={{*/}
+                        {/*            //     label: currentItem?.active ? "active" : "inactive",*/}
+                        {/*            //     value: currentItem?.active*/}
+                        {/*            // }}*/}
+                        {/*            getOptionLabel={option => option.storeName}*/}
+                        {/*            getOptionValue={option => option.id}*/}
+                        {/*            onChange={(val) => {*/}
+                        {/*                formik.setFieldValue("storeId", val.id)*/}
+                        {/*            }}*/}
+                        {/*        />*/}
+                        {/*    </div>*/}
+                        {/*</Col>*/}
                         <Col>
                             <div className="mb-1">
                                 <Label className="form-label" for="adressId">
-                                    Mahsulot manzilini tanlang
+                                    Mahsulot manzilini tanlang *
                                 </Label>
                                 <Select
                                     id="adressId"
@@ -203,10 +208,10 @@ export default function CreateProduct({
                                     }}
                                     placeholder="Tanlang..."
                                     options={addresses}
-                                    // defaultValue={{
-                                    //     label: currentItem?.active ? "active" : "inactive",
-                                    //     value: currentItem?.active
-                                    // }}
+                                    defaultValue={{
+                                        label: addresses.find(item => item.id === product?.adressId)?.adressName,
+                                        value: product?.adressId
+                                    }}
                                     getOptionLabel={option => option.adressName}
                                     getOptionValue={option => option.id}
                                     onChange={(val) => {
@@ -218,7 +223,7 @@ export default function CreateProduct({
                         <Col>
                             <div className="mb-1">
                                 <Label className="form-label" for="productMeasure">
-                                    Mahsulot birligini kiriting
+                                    Mahsulot birligini kiriting *
                                 </Label>
                                 <Select
                                     id="productMeasure"
@@ -229,10 +234,10 @@ export default function CreateProduct({
                                         {value: "dona"},
                                         {value: "litr"}
                                     ]}
-                                    // defaultValue={{
-                                    //     label: currentItem?.active ? "active" : "inactive",
-                                    //     value: currentItem?.active
-                                    // }}
+                                    defaultValue={{
+                                        label: product?.productMeasure,
+                                        value: product?.productMeasure
+                                    }}
                                     getOptionLabel={option => option.value}
                                     getOptionValue={option => option.value}
                                     onChange={(val) => {
--- src/views/products/index.js
+++ src/views/products/index.js
@@ -1,139 +1,60 @@
 import React, {useEffect, useState} from 'react'
-import TableComponent from "../../components/Table"
-import DateFormatClock from "../../components/DateFormatClock"
-import {Plus} from "react-feather"
-import {Button} from "reactstrap"
+import {Card, CardBody, Nav, NavItem, NavLink, TabContent, TabPane} from "reactstrap"
 import {useDispatch, useSelector} from "react-redux"
-import {BiEdit, BiTrash} from "react-icons/bi"
-import {BsEye} from "react-icons/bs"
-import {useLocation} from "react-router-dom"
-import qs from "qs"
-import CreateProduct from "./create-product"
-import {deleteProduct, getProducts, setProduct} from "../../redux/reducers/product"
+import {getStores} from "../../redux/reducers/store"
+import Product from "./product"
 
 export default function Products() {
 
     const dispatch = useDispatch()
-    // const history = useHistory()
-    const location = useLocation()
-    const {
-        products,
-        currentPage,
-        pageCount,
-        limit,
-        totalCount,
-        isLoading
-    } = useSelector(state => state.products)
 
-    const [createModal, setCreateModal] = useState(false)
-    const toggleCreate = () => setCreateModal(!createModal)
+    const {stores} = useSelector(state => state.stores)
 
-    const query = qs.parse(location.search, {ignoreQueryPrefix: true})
+    const [active, setActive] = useState(null)
 
-    useEffect(() => {
-        if (location.search) {
-            dispatch(getProducts({...query}))
-        } else {
-            dispatch(getProducts())
+    const toggle = tab => {
+        if (active !== tab) {
+            setActive(tab)
         }
-    }, [location])
-
-    function editProduct(currentProduct) {
-        dispatch(setProduct(currentProduct))
-        toggleCreate()
     }
 
-    const basicColumns = [
-        {
-            width: '50px',
-            wrap: true,
-            cell: () => <>
-                <BsEye className={'text-success cursor-pointer'} size={20}/>
-            </>
-        },
-        {
-            name: "Nomi",
-            width: '100px',
-            wrap: true,
-            selector: (row) => row.productName
-        },
-        {
-            name: "Narxi",
-            width: '100px',
-            wrap: true,
-            selector: (row) => `${row.productPrice} sum`
-        },
-        {
-            name: "Miqdori",
-            width: '100px',
-            wrap: true,
-            selector: (row) => `${row.productQuantity} ${row.productMeasure}`
-        },
-        {
-            name: "Do'koni",
-            width: '100px',
-            wrap: true,
-            selector: (row) => `${row?.storeId}`
-        },
-        {
-            name: "Manzili",
-            width: '100px',
-            wrap: true,
-            selector: (row) => `${row?.adressId}`
-        },
-        {
-            name: "Modeli",
-            width: '100px',
-            wrap: true,
-            selector: (row) => row.productModel
-        },
-        {
-            name: "Ro'yxatga olingan vaqt",
-            width: '200px',
-            wrap: true,
-            selector: (row) => <DateFormatClock current_date={row.createdAt}/>
-        },
-        {
-            name: "O'zgartirish kiritilgan vaqt",
-            width: '200px',
-            wrap: true,
-            selector: (row) => <DateFormatClock current_date={row.updatedAt}/>
-        },
-        {
-            name: 'Holat',
-            wrap: true,
-            width: '100px',
-            cell: (row) => <div className={`d-flex gap-1`}>
-                <BiEdit className={'text-warning cursor-pointer'} size={20} onClick={() => editProduct(row)}/>
-                <BiTrash className={'text-danger cursor-pointer'} size={20}
-                         onClick={() => dispatch(deleteProduct(row?.id))}/>
-            </div>
-        }
-    ]
+    useEffect(() => {
+        dispatch(getStores())
+    }, [])
 
     return (
         <div>
-            <div className="d-flex items-center justify-content-between">
-                <h4>Ma'lumotlar</h4>
-                <div className="d-flex gap-1">
-                    <Button className="btn-icon" onClick={toggleCreate} outline color="primary">
-                        <Plus size={16}/>
-                    </Button>
-                </div>
-            </div>
-            <div>
-                <TableComponent
-                    progressPending={isLoading}
-                    data={products}
-                    columns={basicColumns}
-                    size={products.length}
-                    limit={limit}
-                    totalPages={pageCount}
-                    currentPage={currentPage}
-                    total_count={totalCount}
-                />
-            </div>
-            <CreateProduct toggleModal={toggleCreate} modal={createModal}/>
+            <Card>
+                <CardBody>
+                    <Nav className="justify-content-center" tabs>
+                        {
+                            stores.map(({
+                                            storeName,
+                                            id
+                                        }, ind) => {
+                                    return (
+                                        <NavItem key={ind}>
+                                            <NavLink
+                                                active={active === id}
+                                                onClick={() => {
+                                                    toggle(id)
+                                                }}
+                                            >
+                                                {storeName}
+                                            </NavLink>
+                                        </NavItem>
+                                    )
+                                }
+                            )
+                        }
+                    </Nav>
+                </CardBody>
+            </Card>
+            <TabContent className="py-50" activeTab={active}>
+                {active !== null && <TabPane tabId={active}>
+                    <Product storeId={active}/>
+                </TabPane>}
+            </TabContent>
         </div>
     )
 }
