5f8eb44141850c8968e3e6008a96b1cdc076e6b6
azadov-azamat <azamatazadov112@gmail.com>
2023-10-02 20:19:07 +0500

    logo delete from spinner

--- src/@core/components/spinner/Fallback-spinner.js
+++ src/@core/components/spinner/Fallback-spinner.js
@@ -1,9 +1,8 @@
 // ** Logo
-import logo from "../../../assets/images/logo/logo.png"
 const SpinnerComponent = () => {
   return (
     <div className='fallback-spinner app-loader'>
-      <img className='fallback-logo' src={logo} alt='logo' />
+      {/*<img className='fallback-logo' src={logo} alt='logo' />*/}
       <div className='loading'>
         <div className='effect-1 effects'></div>
         <div className='effect-2 effects'></div>
