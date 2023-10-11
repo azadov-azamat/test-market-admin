24dae7407c00e878955f61e1662de4b63d45c9fc
azadov-azamat <azamatazadov112@gmail.com>
2023-10-11 17:35:29 +0500

    sf

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import Router from './router'
 
 const App = () => {
 
-    // const dispatch = useDispatch()
-    // const auth = useSelector(state => state.auth)
-    //
-    // useEffect(() => {
-    //     if (!!auth.accessToken) {
-    //         dispatch(loadUser())
-    //     }
-    // }, [auth.accessToken])
-
     return <Router/>
 }
 
