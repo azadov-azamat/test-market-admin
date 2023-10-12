5b5dae934a5b21a2cda24228eace6488b1181a35
azadov-azamat <azamatazadov112@gmail.com>
2023-10-12 19:32:09 +0500

    edit bugs #9"

--- src/views/currencies/index.js
+++ src/views/currencies/index.js
@@ -43,7 +43,7 @@ export default function Currencies({firmId}) {
             }
             setAmount(num)
         }
-    }, [])
+    }, [currencies])
 
     useEffect(() => {
         if (location.search) {
